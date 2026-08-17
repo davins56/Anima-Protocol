289168de3d4eac3260704c89cdc11f056702d13d
Nevhael8 <davins56@gmail.com>
2026-08-17 17:35:11 +0000

    add api keys

---
+++ artifacts/api-server/src/worker.ts
@@ -0,0 +1,10 @@
+import app from "./app";
+
+export default {
+  async fetch(request: Request, env: any, ctx: ExecutionContext): Promise<Response> {
+    return new Response("Worker entry point is alive", {
+      status: 200,
+      headers: { "content-type": "text/plain" },
+    });
+  },
+};
